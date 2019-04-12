daa09c26c86bf13355956227bdaf2c9db418d59b
TEspelien <tespelien@eastsideprep.org>
2019-04-12 15:31:26 -0700

    end of april 12th

--- apweb/chat_te/src/main/java/com/mycompany/chat_te/ChatServer.java
+++ apweb/chat_te/src/main/java/com/mycompany/chat_te/ChatServer.java
@@ -1,15 +1,22 @@
-package com.mycompany.chat_te;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package com.mycompany.chat_te;
+
+import static spark.Spark.*;
 
 /**
  *
  * @author tespelien
  */
 public class ChatServer {
-    
+
+    public static void main(String[] args) {
+        staticFiles.location("static/");
+
+        get("/hello", (req, res) -> "hello world");
+    }
+
 }
